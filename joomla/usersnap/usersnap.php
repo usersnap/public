b1ab755be9b71c6cf8a7f3e2d0eac7dd5461906b
Usersnap <contact@usersnap.com>
2013-08-29 14:24:57 +0200

    added new joomla 2.5/3.0 version

--- joomla/usersnap/usersnap.php
+++ joomla/usersnap/usersnap.php
@@ -1,49 +1,42 @@
 <?php
 // no direct access
-defined( '_JEXEC' ) or die( 'Restricted access' );
- 
-jimport( 'joomla.plugin.plugin' );
+defined('JPATH_BASE') or die;
  
 /**
  * Example system plugin
  */
 class plgSystemUsersnap extends JPlugin
 {
-/**
-* Constructor.
-*
-* @access protected
-* @param object $subject The object to observe
-* @param array   $config  An array that holds the plugin configuration
-* @since 1.0
-*/
-public function __construct( &$subject, $config )
-{
-parent::__construct( $subject, $config ); 
-}
-
-function onBeforeRender() {
-	if ( JFactory::getApplication()->isSite()) {
-		$document = JFactory::getDocument();
-		$apiKey = $this->params->get('api-key', '');
-		if ($apiKey !== "") {
-			$js_code = "
-				var _usersnapconfig = {
-				    apiKey: '".$this->params->get('api-key', '')."',
-				    valign: '".$this->params->get('button-valign', '')."',
-				    halign: '".$this->params->get('button-halign', '')."'
-				}; 
-				(function() {
-				    var s = document.createElement('script');
-				    s.type = 'text/javascript';
-				    s.async = true;
-				    s.src = '//api.usersnap.com/usersnap.js';
-				    var x = document.getElementsByTagName('head')[0];
-				    x.appendChild(s);
-				})();
-			  ";
-			$document->addScriptDeclaration($js_code);
+	function onBeforeRender() {
+		if ( JFactory::getApplication()->isSite()) {
+			$document = JFactory::getDocument();
+			$apiKey = $this->params->get('api-key', '');
+			if ($apiKey !== "") {
+				$js_code = "
+					var _usersnapconfig = {
+					    apiKey: '".$this->params->get('api-key', '')."',
+					    btnText: '".$this->params->get('btn-text', '')."',
+					    shortcut: ".($this->params->get('shortcut', '0')=="1"?"true":"false").",
+					    emailBox: ".($this->params->get('emailbox', '0')=="1"?"true":"false").",
+					    lang: '".$this->params->get('lang', 'en')."',
+					    emailBoxPlaceholder: '".$this->params->get('emailboxph', '')."',
+					    emailBoxValue: '".$this->params->get('emailboxvalue', '')."',
+					    commentBox: ".($this->params->get('commentbox', '0')=="1"?"true":"false").",
+					    commentBoxPlaceholder: '".$this->params->get('commentboxph', '')."',
+					    valign: '".$this->params->get('button-valign', '')."',
+					    halign: '".$this->params->get('button-halign', '')."'
+					}; 
+					(function() {
+					    var s = document.createElement('script');
+					    s.type = 'text/javascript';
+					    s.async = true;
+					    s.src = '//api.usersnap.com/usersnap.js';
+					    var x = document.getElementsByTagName('head')[0];
+					    x.appendChild(s);
+					})();
+				  ";
+				$document->addScriptDeclaration($js_code);
+			}
 		}
 	}
-}
 }
