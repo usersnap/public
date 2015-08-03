8795ff002332f774883c2ff4a60ec9ac638b971f
Usersnap <contact@usersnap.com>
2015-08-03 16:40:01 +0200

    changed drupal to new version + joomla to new version

--- joomla/usersnap/usersnap.php
+++ joomla/usersnap/usersnap.php
@@ -3,7 +3,7 @@
 # usersnap.php - Usersnap - A Screenshot paints a thousand words
 # ------------------------------------------------------------------------
 # author    Usersnap GmbH
-# copyright Copyright (C) 2013 usersnap.com. All Rights Reserved.
+# copyright Copyright (C) 2015 usersnap.com. All Rights Reserved.
 # @license - http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
 # Websites: http://www.usersnap.com
 # Technical Support:  Forum - contact@usersnap.com
@@ -22,24 +22,11 @@ class plgSystemUsersnap extends JPlugin
 			$apiKey = $this->params->get('api-key', '');
 			if ($apiKey !== "") {
 				$js_code = "
-					var _usersnapconfig = {
-					    apiKey: '".$this->params->get('api-key', '')."',
-					    btnText: '".$this->params->get('btn-text', '')."',
-					    shortcut: ".($this->params->get('shortcut', '0')=="1"?"true":"false").",
-					    emailBox: ".($this->params->get('emailbox', '0')=="1"?"true":"false").",
-					    lang: '".$this->params->get('lang', 'en')."',
-					    emailBoxPlaceholder: '".$this->params->get('emailboxph', '')."',
-					    emailBoxValue: '".$this->params->get('emailboxvalue', '')."',
-					    commentBox: ".($this->params->get('commentbox', '0')=="1"?"true":"false").",
-					    commentBoxPlaceholder: '".$this->params->get('commentboxph', '')."',
-					    valign: '".$this->params->get('button-valign', '')."',
-					    halign: '".$this->params->get('button-halign', '')."'
-					}; 
 					(function() {
 					    var s = document.createElement('script');
 					    s.type = 'text/javascript';
 					    s.async = true;
-					    s.src = '//api.usersnap.com/usersnap.js';
+					    s.src = '//api.usersnap.com/load/".$this->params->get('api-key', '').".js';
 					    var x = document.getElementsByTagName('head')[0];
 					    x.appendChild(s);
 					})();
