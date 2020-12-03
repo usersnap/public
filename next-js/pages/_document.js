fd38c9234136ceb4942ee7fa3fdffabacffe5206
gabberr <gaber.setina@gmail.com>
2020-12-03 11:20:04 +0100

    replace api keys

--- next-js/pages/_document.js
+++ next-js/pages/_document.js
@@ -1,8 +1,8 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 // Replace with the API keys from your widget installation page
-const USERSNAP_GLOBAL_API_KEY = '0ec10f93-e744-4e1e-ac5c-d114ab77e4e4'
-const USERSNAP_API_KEY = '942dc29d-912c-42be-ac22-495d98115363'
+const USERSNAP_GLOBAL_API_KEY = '<USERSNAP_GLOBAL_API_KEY>'
+const USERSNAP_API_KEY = '<USERSNAP_API_KEY>'
 
 export default class MyDocument extends Document {
   render() {
