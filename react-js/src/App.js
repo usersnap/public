7ae0352eb4fb639b0fb36736eaa138d5ba4bbcec
gabberr <gaber.setina@gmail.com>
2020-12-03 15:36:44 +0100

    add react usersnap example

--- react-js/src/App.js
+++ react-js/src/App.js
@@ -1,8 +1,29 @@
+import Helmet from 'react-helmet'
+
 import logo from './logo.svg';
 import './App.css';
 
+// Replace with the API keys from your widget installation page
+const USERSNAP_GLOBAL_API_KEY = "<USERSNAP_GLOBAL_API_KEY>"
+const USERSNAP_API_KEY = "<USERSNAP_API_KEY>"
+
 function App() {
   return (
+    <>
+    <Helmet>
+      <script type="text/javascript">
+        {`
+            window.onUsersnapCXLoad = function(api) {
+              api.init();
+              api.show('${USERSNAP_API_KEY}') 
+            }
+            var script = document.createElement('script');
+            script.defer = 1;
+            script.src = 'https://widget.usersnap.com/global/load/${USERSNAP_GLOBAL_API_KEY}?onload=onUsersnapCXLoad';
+            document.getElementsByTagName('head')[0].appendChild(script);
+        `}
+      </script>
+    </Helmet>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,6 +40,7 @@ function App() {
         </a>
       </header>
     </div>
+    </>
   );
 }
 
